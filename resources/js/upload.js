bbff333ae079967c19cf93e12eeea8f4b9884660
Barbara Ristau <ristaub@gmail.com>
2018-04-20 13:01:05 -0700

    add upload section

---
+++ resources/js/upload.js
@@ -0,0 +1,78 @@
+//https://developer.mozilla.org/en-US/docs/Web/API/File/Using_files_from_web_applications
+//https://www.smashingmagazine.com/2018/01/drag-drop-file-uploader-vanilla-js/
+
+
+let dropArea = document.getElementById('drop-area');['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, preventDefaults, false)
+})
+
+
+// ************* Functions for Drag and Drop
+
+
+function preventDefaults (e) {
+    e.preventDefault()
+    e.stopPropagation()
+}
+
+;['dragenter', 'dragover'].forEach(eventName => {
+    dropArea.addEventListener(eventName, highlight, false)
+})
+
+;['dragleave', 'drop'].forEach(eventName => {
+    dropArea.addEventListener(eventName, unhighlight, false)
+})
+
+function highlight(e) {
+    dropArea.classList.add('highlight')
+}
+
+function unhighlight(e) {
+    dropArea.classList.remove('highlight')
+}
+
+dropArea.addEventListener('drop', handleDrop, false)
+
+function handleDrop(e) {
+    let dt = e.dataTransfer
+    let files = dt.files
+
+    handleFiles(files)
+}
+
+function handleFiles(files) {
+    files = [...files]
+    files.forEach(uploadFile)
+    files.forEach(previewFile)
+}
+
+function previewFile(file) {
+    let reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onloadend = function() {
+        let img = document.createElement('img')
+        img.src = reader.result
+        document.getElementById('gallery').appendChild(img)
+    }
+}
+
+function uploadFile(file) {
+    let url = 'YOUR URL HERE'
+    let formData = new FormData()
+
+    formData.append('file', file)
+
+    fetch(url, {
+        method: 'POST',
+        body: formData
+    })
+        .then(() => { /* Done. Inform the user */ })
+        .catch(() => { /* Error. Inform the user */ })
+}
+
+
+
+
+
+
+
